4bac6ccb9c77ed49f6cda8daaf4ed34d5b9a6105
Mihály Vesztergombi <mihaly.veszter@gmail.com>
2023-04-07 23:27:00 +0200

    print message in sample

--- samples/exit.s
+++ samples/exit.s
@@ -5,6 +5,20 @@
 section .text
 global _start
 _start:
-        mov edi, 42  ; return code 42
-        mov eax, 60  ; `_exit` syscall
-        syscall
+    sub rsp, 10
+    mov byte [rsp+0], 'H'
+    mov byte [rsp+1], 'i'
+    mov byte [rsp+2], '!'
+    mov byte [rsp+3], 10
+
+    mov edi, 1  ; stdout
+    mov rsi, rsp
+    mov rdx, 4
+    mov eax, 1  ; write
+    syscall
+    
+    add rsp, 10
+
+    xor edi, edi
+    mov eax, 60  ; exit
+    syscall
